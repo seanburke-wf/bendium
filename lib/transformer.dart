68596b8eb03205d4e82e3601b33a58aadf81c5e3
George Lesica <george.lesica@workiva.com>
2017-09-01 11:33:21 -0600

    Copy manifest version in the transformer

--- lib/transformer.dart
+++ lib/transformer.dart
@@ -1,19 +1,30 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:barback/barback.dart';
+import 'package:pubspec/pubspec.dart';
 
 import 'src/actions.dart' show actions;
 
 // TODO: This could update the manifest version from the pubspec as well.
 
-class ManifestCommands extends Transformer {
-  ManifestCommands.asPlugin();
+class ManifestTransformer extends Transformer {
+  ManifestTransformer.asPlugin();
 
   Future apply(Transform transform) async {
     var stringContent = await transform.primaryInput.readAsString();
     var jsonContent = JSON.decode(stringContent);
 
+    // Version
+    var pubspec = await PubSpec.load(Directory.current);
+    var version = pubspec.version;
+
+    if (jsonContent is Map) {
+      jsonContent['version'] = version.toString();
+    }
+
+    // Commands
     if (jsonContent is Map) {
       Map commands;
       if (jsonContent['commands'] is Map) {
